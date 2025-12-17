8e18f31a89ccdf67733aaf6a9655fe71ce1ec443
bohoplatao <cuhopyi2004@gmail.com>
2025-12-17 22:23:13 +0700

    [dev/Hop] 1712 commit Room

---
+++ Project.Application/DTOs/RoomDTO/RoomResponseDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Application.DTOs.RoomDTO
+{
+    public class RoomResponseDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public int Capacity { get; set; }
+
+        public string Address { get; set; } = null!;
+
+        public bool? Status { get; set; }
+    }
+}
---
+++ Project.Domain/Interfaces/Repositories/IRoomRepository.cs
@@ -0,0 +1,16 @@
+﻿using Project.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Domain.Interfaces.Repositories
+{
+    public interface IRoomRepository
+    {
+        Task<List<Room>> GetAllAsync();
+        Task<Room?> GetByIdAsync(int id);
+        Task AddAsync(Room room);
+    }
+}
